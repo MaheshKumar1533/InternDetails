92c7252b208635a60e7d211c396e26d58b62cf75
hemanth12 <hv767197@gmail.com>
2024-02-11 18:21:26 +0530

    login authenacation

--- Internships/views.py
+++ Internships/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
 def departments(request):
     return render(request, "Departments.html")
 
+
 def login(request):
-    return render(request, "login.html")
+    user = request.POST.get("username")
+    password = request.POST.get("password")
+    User =authenticate(username=user,password =password)
+    if user==None or password==None:
+        return render(request, "login.html")
+    if User is not None:
+        return render(request, "departments.html", context={'User':User})
+    elif context['authentication']==0:
+        return redirect(request, "login.html", context={'authentication':1})
+    else:
+        print("Failed")
