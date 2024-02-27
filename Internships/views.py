9bcf33ee1cc5549313dd3e5ad4e01f7c596a61f4
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2024-02-27 14:51:31 +0530

    Commit from Mahesh

--- Internships/views.py
+++ Internships/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth import authenticate,login,logout
 from .forms import StudentForm
 
 User = 0  #global user to handle the login through the dashboard
+#primary Dashboard without login
+def primaryDashboard(request):
+    return render(request,"primaryDashboard.html")
 
 @login_required #login id mandatory to access the exclusive dashboard
 def departments(request):
@@ -19,24 +22,35 @@ def custom_login(request, context={'authentication':0}):
     User =authenticate(request,username=user,password =password)
     #to not display failure method primarly if np details provided
     if user==None or password==None:
-        return render(request, "login.html")
+        return render(request, "custom_login.html")
     if User is not None:
         login(request,User)
         return render(request, "departments.html", context={'User':User, 'departments':depts.objects.all().values()})
     elif context['authentication']==0:
-        return render(request, "login.html", context={'authentication':1})
+        return render(request, "custom_login.html", context={'authentication':1})
     else:
         print("Failed")
-
-#primary Dashboard without login
-def primaryDashboard(request):
-    return render(request,"primaryDashboard.html")
-
 
 #logout view
 def custom_logout(request):
     logout(request)
     return redirect("login",)
+def register_form(request):
+    return render(request, "Registration_form.html")
+def create_student(request):
+    global User
+    if request.method == 'POST':
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            print("Form created")
+            return render(request, 'create_student.html', {"form": form})
+    else:
+        form = StudentForm()
+    return render(request, 'create_student.html', {'form': form})
+
+
+
 
 #student details dashboard
 def department(request):
@@ -53,17 +67,7 @@ def Details(request):
     print(Students)
     return render(request, "Details.html", context={'User': User, 'Students': Students, 'internships': internships})
 
-def create_student(request):
-    global User
-    if request.method == 'POST':
-        form = StudentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            print("Form created")
-            return render(request, 'create_student.html', {"form": form})
-    else:
-        form = StudentForm()
-    return render(request, 'create_student.html', {'form': form})
+
 
 def bulk_data_input(request):
     if request.method == 'POST':
@@ -79,3 +83,6 @@ def bulk_data_input(request):
     else:
         form = BulkDataForm()
     return render(request, 'bulk_update.html', {'form': form})
+
+def addInternship(request):
+    return render(request,'intern_details.html')
